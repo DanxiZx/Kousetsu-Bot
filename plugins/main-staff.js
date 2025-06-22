c545e4cf66c60cb94c5ae50015aa3659ba34c16b
𝙈𝙇𝘽𝙏-𝙭𝙯 <diegolesimarcelino@gmail.com>
2025-06-22 12:56:29 -0400

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -3,42 +3,10 @@ let staff = `🚩 *EQUIPO DE AYUDANTES*
 🤖 *Bot:* ${global.botname}
 🪐 *Versión:* ${global.vs}
 
-• 𝐃𝐚𝐫𝐤𝐂𝐨𝐫𝐞
-🎩 *Rol:* Developer
-💛 *Número:* Wa.me/51968382008
+• ᴀʜᴛᴏᴍ 
+👤 *Rol:* Developer
+🧾 *Número:* Wa.me/51968382008
 
-• 𝐃𝐞𝐬𝐭𝐫𝐨𝐲
-🍭 *Rol* Developer
-💛 *Numero:* Wa.me/584120346669
-
-• 𝐏𝐫𝐚𝐤 𝐇𝐚𝐫𝐩𝐞𝐫 
-🎩 *Rol:* Developer
-💛 *Número:* Wa.me/584241836217
-
-• 𝐈𝐬𝐚𝐛𝐞𝐥 (isa)
-🍭 *Rol:* Mod
-💛 *Número:* Wa.me/529831715910
-
-
-• 𝐌𝐨𝐫𝐚𝐥𝐞𝐬
-🍭 *Rol:* Mod
-💛 *Numero:* Wa.me/573007796996
-
-• 𝐃𝐞𝐲𝐥𝐢𝐧 
-🎩 *Rol:* Mod
-💛 *Número:* Wa.me/50488198573
-
-• 𝐎𝐫𝐥𝐚𝐧𝐝𝐨
-🍭 *Rol:* Mod
-💛 *Número:* Wa.me/522731590195
-
-• 𝐒𝐄𝐈𝐒𝐇𝐈𝐑𝐎 - 𝐍𝐀𝐆𝐈
-🎩 *Rol:* Mod
-💛 *Número:* Wa.me/51920227615
-
-• 𝐀𝐍𝐆𝐄𝐋
-🍭 *Rol:* Mod
-💛 *Numero:* Wa.me/50368471855`
 await conn.sendFile(m.chat, 'https://cdnmega.vercel.app/media/c4hhgZgD@fGOHhRAM1CD-3_cpAQk-Q86yQnQLGHYKZ1M0P_heI9s', 'staff.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
@@ -46,7 +14,7 @@ contextInfo: {
 externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: false,
-title: `🎩 STAFF OFICIAL🌟`,
+title: `sᴛᴀғғ ᴏғɪᴄɪᴀʟ 🌠`,
 body: dev,
 mediaType: 1,
 sourceUrl: redes,

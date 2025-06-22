ec0d2afa6023b296ae4c84765f0bd2ea34f2c5a4
𝙈𝙇𝘽𝙏-𝙭𝙯 <diegolesimarcelino@gmail.com>
2025-06-22 12:43:37 -0400

    Actualizar gc-setdespedida.js

--- plugins/gc-setdespedida.js
+++ plugins/gc-setdespedida.js
@@ -6,7 +6,7 @@
 
 let handler = async (m, { conn, text, isRowner }) => {
   if (!text) {
-    return m.reply('🍭 Ingresa Un Mensaje Para La Despedida!\n> Ejemplo: `.setdespedida ¡Se fue😹!`');
+    return m.reply('🌟 Ingresa Un Mensaje Para La Despedida!\n> Ejemplo: `.setdespedida ¡Se fue😹!`');
   }
 
   const despMessage = text.trim();
@@ -14,10 +14,10 @@ let handler = async (m, { conn, text, isRowner }) => {
   try {
     global.db.data.chats[m.chat].despMessage = despMessage;
 
-    m.reply(`🍭 El mensaje de despedida del grupo ahora : ${despMessage}`);
+    m.reply(`💋 El mensaje de despedida del grupo ahora : ${despMessage}`);
   } catch (error) {
     console.error(error);
-    m.reply('🍭 Hubo un error al intentar cambiar el mensaje de despedida.');
+    m.reply('⚠️ Hubo un error al intentar cambiar el mensaje de despedida.');
   }
 };
 

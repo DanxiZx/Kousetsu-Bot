3fd47582fd3508f90d234c78a2bab4f20cd49fb7
𝙈𝙇𝘽𝙏-𝙭𝙯 <diegolesimarcelino@gmail.com>
2025-06-22 13:04:08 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -2,14 +2,14 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
 
-if (!args[0]) return m.reply(`🍭 Ingresa Un Link De YouTube.`);
+if (!args[0]) return m.reply(`🌸 Ingresa Un Link De YouTube.`);
 
 m.react('🕒')
 let api = await(await fetch(`https://api.neoxr.eu/api/youtube?url=${args[0]}&type=audio&quality=128kbps&apikey=GataDios`)).json();
 
 // if (!api?.result?.dl_url) return m.reply('No Se  Encontraron Resultados');
 
-/* let txt = `「✦」𝗧𝗶𝘁𝘂𝗹𝗼: ${api.result.result.title}`;
+/* let txt = `「🌸」𝗧𝗶𝘁𝘂𝗹𝗼: ${api.result.result.title}`;
 conn.reply(m.chat, txt, m, rcanal);
 */
 

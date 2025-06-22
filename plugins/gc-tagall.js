ed29169e0fe1f62e7c52dc79206c7f0f52a14668
𝙈𝙇𝘽𝙏-𝙭𝙯 <diegolesimarcelino@gmail.com>
2025-06-22 12:45:58 -0400

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,7 +5,7 @@ import PhoneNumber from 'awesome-phonenumber';
 const handler = async (m, { participants, args }) => {
   const pesan = args.join` `;
   const oi = `*» INFO :* ${pesan}`;
-  let mensajes = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ \`⡞᪲=͟͟͞🄲ꭈׁׅo͓̽ᨰׁׅʙo͓̽tׁׅ ≼᳞ׄ\` ꒱  ۟  𝅄 ┄\n`;
+  let mensajes = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 📢\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ \`ʀɪᴀsᴇᴍᴘᴀɪ-ɪɴᴠᴏᴄᴀʀ‼️` ꒱  ۟  𝅄 ┄\n`;
 
   for (const mem of participants) {
     let numero = PhoneNumber('+' + mem.id.replace('@s.whatsapp.net', '')).getNumber('international');
@@ -13,7 +13,7 @@ const handler = async (m, { participants, args }) => {
     let response = await fetch(api);
     let json = await response.json();
 
-    let paisdata = json.result ? json.result.emoji : '🍫';
+    let paisdata = json.result ? json.result.emoji : '💗';
     mensajes += `${paisdata} @${mem.id.split('@')[0]}\n`;
   }
 
